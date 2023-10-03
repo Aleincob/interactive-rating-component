8fb54d5a81bfa40c72ea7c118be2f5bd89ba6534
Alejandro Jacobo <alecaperez13@gmail.com>
2023-10-02 19:55:12 -0500

    Creamos los primeros archivos html y js para el proyecto

---
+++ project/main.js
@@ -0,0 +1,3 @@
+//to do List:
+// 1: Hacer 5 botones para seleccionar el rating dado (del 1 al 5)
+// 2: Hacer un boton submit con funcion upload para cambiar la vista al de agradecimiento junto con el rating seleccionado
